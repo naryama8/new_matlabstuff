5874ca7b052d55db30ea463a972d38e3b9d675ad
Rafi <rafi.naryama@ui.ac.id>
2026-03-15 11:39:35 +0700

    pmsm added

---
+++ skml_tugas4/pmsm.c
@@ -0,0 +1,57 @@
+#define S_FUNCTION_LEVEL 2
+#define S_FUNCTION_NAME STRUKTUR_C
+#include "simstruc.h"
+#include <math.h>
+
+#define U(element) (*uPtrs[element]) /*Pointer to Input Port0*/
+
+static void mdlInitializeSizes(SimStruct *S){
+    ssSetNumContStates(S, 1);
+    if (!ssSetNumInputPorts(S, 1)) return;
+    ssSetInputPortWidth(S, 0, 1);
+    ssSetInputPortDirectFeedThrough(S, 0, 1);
+    ssSetInputPortOverWritable(S, 0, 1);
+    if (!ssSetNumOutputPorts(S, 1)) return;
+    ssSetOutputPortWidth(S, 0, 1);
+    ssSetNumSampleTimes(S, 1);
+
+    ssSetOptions(S, SS_OPTION_EXCEPTION_FREE_CODE); }
+
+static void mdlInitializeSampleTimes(SimStruct *S) {
+    ssSetSampleTime(S, 0, CONTINUOUS_SAMPLE_TIME);
+    ssSetOffsetTime(S, 0, 0.0); }
+
+#define MDL_INITIALIZE_CONDITIONS
+static void mdlInitializeConditions(SimStruct *S) {
+
+    real_T *X0 = ssGetContStates(S);
+    int_T nStates = ssGetNumContStates(S);
+    int_T i;
+
+    /* initialize the states to 0.0 */
+    for (i=0; i < nStates; i++) {X0[i] = 0.0;} }
+
+static void mdlOutputs(SimStruct *S, int_T tid) {
+    real_T *Y = ssGetOutputPortRealSignal(S,0);
+    real_T *X = ssGetContStates(S);
+    InputRealPtrsType uPtrs = ssGetInputPortRealSignalPtrs(S,0);
+
+}
+
+#define MDL_DERIVATIVES
+static void mdlDerivatives(SimStruct *S) {
+    real_T *dX = ssGetdX(S);
+    real_T *X = ssGetContStates(S);
+    InputRealPtrsType uPtrs = ssGetInputPortRealSignalPtrs(S,0);
+
+}
+
+static void mdlTerminate(SimStruct *S)
+{} /*Keep this function empty since no memory is allocated*/
+
+#ifdef MATLAB_MEX_FILE
+/* Is this file being compiled as a MEX-file? */
+#include "simulink.c" /* MEX-file interface mechanism */
+#else
+#include "cg_sfun.h" /*Code generation registration function*/
+#endif
